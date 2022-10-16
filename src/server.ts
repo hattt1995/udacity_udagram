7db2b0bbde6a50694b641e4323611760e41c665e
Ha Tran <hattt.1995@gmail.com>
2022-10-17 01:23:42 +0700

    implement TODO1

--- src/server.ts
+++ src/server.ts
@@ -26,9 +26,38 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
   /**************************************************************************** */
+  app.get( "/filteredimage/", async ( req, res ) => {
 
+    // 1. validate the image_url query
+    let {image_url } = req.query;
+    if (!image_url) {
+      return res.status(400).send(`Query param 'image_url' is required`);
+    }
+
+    try {
+      // 2. call filterImageFromURL(image_url) to filter the image
+      const filteredImagePath = await filterImageFromURL(image_url);
+
+      // 3. send the resulting file in the response
+      res.status(200).sendFile(filteredImagePath, function (err) {
+        if (err) {
+          throw "Send file fail";
+        } else {
+          // 4. deletes any files on the server on finish of the response
+          deleteLocalFiles([filteredImagePath]);
+        }
+      });
+      
+    } catch (error) {
+      const msg = (error as Error).message;
+      if(msg != null && msg.includes("404")) {
+        return res.status(404).send(`Image Not found`);
+      } else {
+        return res.status(500).send(`Internal Server Error`);
+      }
+    }
+  } );
   //! END @TODO1
   
   // Root Endpoint
